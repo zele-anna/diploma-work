16c0e9633b65a16b523211e145af5ff8503c3c05
Anna <azeleranskaya@gmail.com>
2025-10-24 11:39:57 +0300

    change redis port

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import include, path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from config import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -23,4 +25,5 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('', include('docs_manager.urls', namespace='docs_manager')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
